fba95610411f9f4aa05bd7f54d2026c489c60c85
Munawwar <munawwarfiroz@hotmail.com>
2022-09-01 00:09:53 +0530

    readme update

--- README.md
+++ README.md
@@ -17,38 +17,34 @@ $ npm install chartist --save
 ### Usage
 
 ```JavaScript
-import { ChartistBarChart } from 'react-chartist';
+import { ChartistBarChart } from 'preact-chartist';
 
-class Bar extends React.Component {
-  render() {
+const data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
 
-    var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-      series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
-      ]
-    };
-
-    var options = {
-      high: 10,
-      low: -10,
-      axisX: {
-        labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ? value : null;
-        }
-      }
-    };
-
-    return (
-      <div>
-        <ChartistBarChart data={data} options={options} />
-      </div>
-    )
+const options = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
   }
+};
+
+function Bar() {
+  return (
+    <div>
+      <ChartistBarChart data={data} options={options} />
+    </div>
+  )
 }
 
 ReactDOM.render(<Bar />, document.body)
-
 ```
 
 ### Options
--- example/main.jsx
+++ example/main.jsx
@@ -1,39 +1,35 @@
 // @ts-check
-import { h, render, Component } from 'preact';
+import { h, render } from 'preact';
 import { ChartistBarChart } from '../index';
 
-class Bar extends Component {
-  render() {
+const data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
 
-    var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-      series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
-      ]
-    };
-
-    var options = {
-      high: 10,
-      low: -10,
-      axisX: {
-        labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ? value : null;
-        }
-      }
-    };
-
-    var aspectRatio = 'ct-octave';
-
-    return (
-      <div>
-        <ChartistBarChart options={options} className={aspectRatio} data={data} />
-      </div>
-    )
+const options = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
   }
+};
+
+function Bar() {
+  var aspectRatio = 'ct-octave';
+  return (
+    <div>
+      <ChartistBarChart options={options} className={aspectRatio} data={data} />
+    </div>
+  )
 }
 
 render(
   <Bar />,
   // @ts-ignore
-  document.getElementById('react-chart'),
+  document.getElementById('preact-chart'),
 )
